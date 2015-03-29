4ae34b251fac0922839f4f629aec4e4756e5033a
Stepan <xevinbox@yandex.ru>
2015-03-29 13:46:58 +0500

    add script and styles;

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # treemenu.js
 treeMenu - simple jQuery plugin. Add togglers to ul > li > *  structure
+
+Usage:
+0) include jQuery
+    <script src="jquery.min.js" type="text/javascript"></script>
+
+1) include treeMenu script and styles in your document
+    <script src="treemenu.js" type="text/javascript"></script>
+    <link href="treemenu.css" rel="stylesheet" type="text/css" />
+
+2) Attach treeMenu when the document is loaded
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("ul.mytree").treemenu();
+    });
+   </script>
---
+++ jquery.treemenu.js
@@ -0,0 +1,69 @@
+/*
+ treeMenu - jQuery plugin
+ version: 0.4
+ 
+ Copyright 2014 Stepan Krapivin
+
+*/
+
+(function($){
+    $.fn.openActive = function(activeSel) {
+        activeSel = activeSel || ".active";
+
+        var c = this.attr("class");
+
+        this.find(activeSel).each(function(){
+            var el = $(this).parent();
+            while (el.attr("class") !== c) {
+                if(el.prop("tagName") === 'UL') {
+                    el.show();
+                } else if (el.prop("tagName") === 'LI') {
+                    el.removeClass('tree-closed');
+                    el.addClass("tree-opened");
+                }
+
+                el = el.parent();
+            }
+        });
+
+        return this;
+    }
+
+    $.fn.treemenu = function(options) {
+        options = options || {};
+        options.delay = options.delay || 0;
+        options.openActive = options.openActive || false;
+        options.activeSelector = options.activeSelector || "";
+
+        this.find("> li").each(function() {
+            e = $(this);
+            var subtree = e.find('> ul');
+            var toggler = $('<span>');
+            toggler.addClass('toggler');
+
+            e.prepend(toggler);
+            if(subtree.length > 0) {
+                subtree.hide();
+
+                e.addClass('tree-closed');
+
+                e.find(toggler).click(function() {
+                    var li = $(this).parent('li');
+                    li.find('> ul').toggle(options.delay);
+                    li.toggleClass('tree-opened');
+                    li.toggleClass('tree-closed');
+                });
+
+                $(this).find('> ul').treemenu(options);
+            } else {
+                $(this).addClass('tree-empty');
+            }
+        });
+
+        if (options.openActive) {
+            this.openActive(options.activeSelector);
+        }
+
+        return this;
+    }
+})(jQuery);
