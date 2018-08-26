57e1e07b2f1ba87d45c71df2795336508c38c8ec
Stepan Krapivin <xevinbox@yandex.ru>
2018-08-26 23:10:34 +0500

    fix selector

--- jquery.treemenu.js
+++ jquery.treemenu.js
@@ -6,81 +6,81 @@
 
 */
 (function($) {
-    $.fn.treemenu = function(options) {
-        options = options || {};
-        options.delay = options.delay || 0;
-        options.openActive = options.openActive || false;
-        options.closeOther = options.closeOther || false;
-        options.activeSelector = options.activeSelector || "active";
+  $.fn.treemenu = function(options) {
+    options = options || {};
+    options.delay = options.delay || 0;
+    options.openActive = options.openActive || false;
+    options.closeOther = options.closeOther || false;
+    options.activeSelector = options.activeSelector || "active";
 
-        this.addClass("treemenu");
+    this.addClass("treemenu");
 
-        if (!options.nonroot) {
-            this.addClass("treemenu-root");
-        }
+    if (!options.nonroot) {
+      this.addClass("treemenu-root");
+    }
 
-        options.nonroot = true;
+    options.nonroot = true;
 
-        this.find("> li").each(function() {
-            e = $(this);
-            var subtree = e.find('> ul');
-            var button = e.find('.toggler').eq(0);
+    this.find("> li").each(function() {
+      e = $(this);
+      var subtree = e.find('> ul');
+      var button = e.find('.toggler').eq(0);
 
-            if (button.length == 0) {
-                // create toggler
-                var button = $('<span>');
-                button.addClass('toggler');
-                e.prepend(button);
-            }
+      if (button.length == 0) {
+        // create toggler
+        var button = $('<span>');
+        button.addClass('toggler');
+        e.prepend(button);
+      }
 
-            if (subtree.length > 0) {
-                subtree.hide();
-                e.addClass('tree-closed');
+      if (subtree.length > 0) {
+        subtree.hide();
+        e.addClass('tree-closed');
 
-                e.find(button).click(function() {
-                    var li = $(this).parent('li');
+        e.find(button).click(function() {
+          var li = $(this).parent('li');
 
-                    if (options.closeOther && li.hasClass('tree-closed')) {
-                        var siblings = li.parent('ul').find("li:not(.tree-empty)");
-                        siblings.removeClass("tree-opened");
-                        siblings.addClass("tree-closed");
-                        siblings.removeClass(options.activeSelector);
-                        siblings.find('> ul').slideUp(options.delay);
-                    }
+          if (options.closeOther && li.hasClass('tree-closed')) {
+            var siblings = li.parent('ul').find("li:not(.tree-empty)");
+            siblings.removeClass("tree-opened");
+            siblings.addClass("tree-closed");
+            siblings.removeClass(options.activeSelector);
+            siblings.find('> ul').slideUp(options.delay);
+          }
 
-                    li.find('> ul').slideToggle(options.delay);
-                    li.toggleClass('tree-opened');
-                    li.toggleClass('tree-closed');
-                    li.toggleClass(options.activeSelector);
-                });
-
-                $(this).find('> ul').treemenu(options);
-            } else {
-                $(this).addClass('tree-empty');
-            }
+          li.find('> ul').slideToggle(options.delay);
+          li.toggleClass('tree-opened');
+          li.toggleClass('tree-closed');
+          li.toggleClass(options.activeSelector);
         });
 
-        if (options.openActive) {
-            var cls = this.attr("class");
+        $(this).find('> ul').treemenu(options);
+      } else {
+        $(this).addClass('tree-empty');
+      }
+    });
 
-            this.find(options.activeSelector).each(function(){
-                var el = $(this).parent();
+    if (options.openActive) {
+      var cls = this.attr("class");
 
-                while (el.attr("class") !== cls) {
-                    el.find('> ul').show();
-                    if(el.prop("tagName") === 'UL') {
-                        el.show();
-                    } else if (el.prop("tagName") === 'LI') {
-                        el.removeClass('tree-closed');
-                        el.addClass("tree-opened");
-                        el.show();
-                    }
+      this.find('.'+options.activeSelector).each(function(){
+        var el = $(this).parent();
 
-                    el = el.parent();
-                }
-            });
+        while (el.attr("class") !== cls) {
+          el.find('> ul').show();
+          if(el.prop("tagName") === 'UL') {
+            el.show();
+          } else if (el.prop("tagName") === 'LI') {
+            el.removeClass('tree-closed');
+            el.addClass("tree-opened");
+            el.show();
+          }
+
+          el = el.parent();
         }
-
-        return this;
+      });
     }
+
+    return this;
+  }
 })(jQuery);
